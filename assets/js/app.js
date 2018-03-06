9a99b5b50a65d490a89c4ba261da3b285f6649bd
KY <kevinxy00@gmail.com>
2018-03-06 05:00:48 -0500

    wrapped plot in function, started on multi axis selection

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@
 // Create a scatter plot with D3.js.
 
 // set margins
-var margin = {top: 20, right: 20, bottom: 30, left: 40};
+var margin = {top: 20, right: 20, bottom: 100, left: 100};
 var width = 800 - margin.left - margin.right;
 var height = 500 - margin.top - margin.bottom;
 
@@ -22,6 +22,70 @@ var svg = d3.select("body").append("svg")
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+// x axis title/label
+// divorced rate
+svg.append("text")
+    .attr("class", "axis-text active")
+    .attr("id", "x")
+    .attr("x", width/2 -50)
+    .attr("y", height + 35)
+    .attr("style", "font-weight:bold")
+    .attr("data-axis-name", "Divorced_total_2014")
+    .text("Divorced rate (%)");
+// poverty rate 
+svg.append("text")
+    .attr("class", "axis-text")
+    .attr("id", "x")
+    .attr("x", width/2 -50)
+    .attr("y", height + 55)
+    .attr("style", "font-weight:bold")
+    .attr("data-axis-name", "Poverty_rate")
+    .text("Poverty rate (%)");
+// unemployment rate
+svg.append("text")
+    .attr("class", "axis-text")
+    .attr("id", "x")
+    .attr("x", width/2 -50)
+    .attr("y", height + 75)
+    .attr("style", "font-weight:bold")
+    .attr("data-axis-name", "Unemployment_rate")
+    .text("Unemployment rate (%)");
+
+// y axis title/label
+// depression rate
+svg.append("text")
+    .attr("class", "axis-text active")
+    .attr("id", "y")
+    .attr("transform", "rotate(-90)")
+    .attr("x", -height/2 - 50)
+    .attr("y", -40)
+    .attr("dy", ".71em")
+    .attr("style", "font-weight:bold")
+    .attr("data-axis-name", "Depression_total_2014")
+    .text("Depression rate (%)");
+// heart attack rate
+svg.append("text")
+    .attr("class", "axis-text")
+    .attr("id", "y")
+    .attr("transform", "rotate(-90)")
+    .attr("x", -height/2 - 50)
+    .attr("y", -60)
+    .attr("dy", ".71em")
+    .attr("style", "font-weight:bold")
+    .attr("data-axis-name", "heart_attack_rate")
+    .text("Heart attack rate (%)");
+//Dr visits within last year rate
+svg.append("text")
+    .attr("class", "axis-text")
+    .attr("id", "y")
+    .attr("transform", "rotate(-90)")
+    .attr("x", -height/2 - 110)
+    .attr("y", -80)
+    .attr("dy", ".71em")
+    .attr("style", "font-weight:bold")
+    .attr("data-axis-name", "Dr_visit_within_last_year")
+    .text("% who visited doctor within the last year");
+
 // pull in the data
 var csv_data = "Data/data.csv";
 
@@ -32,83 +96,85 @@ d3.csv(csv_data, function(error, data) {
         // make into a number in case it comes in as string
         d.Divorced_total_2014 = +d.Divorced_total_2014;
         d.Depression_total_2014 = +d.Depression_total_2014;
-        console.log(d);
+        d.Poverty_rate = +d.Poverty_rate;
+        d.heart_attack_rate = +d.heart_attack_rate;
+        d.Unemployment_rate = +d.Unemployment_rate;
+        d.Dr_visit_within_last_year = +d.Dr_visit_within_last_year;
     });
 
-    // set domain
-    xScale.domain(d3.extent(data, function(d) {
-        return d.Divorced_total_2014;
-    }));
-    yScale.domain(d3.extent(data, function(d){
-        return d.Depression_total_2014;
-    }));
+    // get active data-axis-name
+    var activeX = d3
+        .select("#x")
+        .filter(".active").attr("data-axis-name");
+    var activeY = d3
+        .select("#y")
+        .filter(".active").attr("data-axis-name");
 
-    // append svg for x and y axes
-    svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
+    console.log("activeX: " + activeX + "; activeY: " + activeY);
+    
+    // plot first chart 
+    plot_data(activeX, activeY);
 
-      svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis);
+    // *** Function that creates graph for selected x and y
+    function plot_data(x_data, y_data) {
 
+        // set domain
+        xScale.domain(d3.extent(data, function(d) {
+            return d[x_data];
+        }));
+        yScale.domain(d3.extent(data, function(d){
+            return d[y_data];
+        }));
 
-    // scatterplot dots
-    svg.selectAll(".dot")
-        .data(data)
-        .enter().append("circle")
-        .attr("class", "dot")
-        .attr("r", 10)
-        .attr("cx", function(d){
-            return xScale(d.Divorced_total_2014);
-        })
-        .attr("cy", function(d){
-            return yScale(d.Depression_total_2014)
-        })
-        .attr("fill", "red")
-        .style("opacity", 0.5);
+        // append svg for x and y axes
+        svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
 
-    // append text to each point
-    var text = svg.selectAll("dot")
-        .data(data)
-        .enter()
-        .append("text");
-
-    var textLabels = text
-        .attr("class", "label")
-        .attr("x", function(d){
-            console.log("text x: " + Number(d.Divorced_total_2014));
-            return xScale(d.Divorced_total_2014)-8;
-        })
-        .attr("y", function(d){
-            return yScale(d.Depression_total_2014)+4;
-        })
-        .text(function(d){
-            console.log(d.State);
-            return d.State;
-        })
-        .style("font-size", "10px")
-        .style("font-weight", "bold")
-        .style("font-family", "verdana")
-        .style("opacity", 0.6);
+        svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
 
 
-    // x axis title/label
-    var xLabel = svg.append("text")
-        .attr("class", "label")
-        .attr("x", width/2)
-        .attr("y", height + 28)
-        .attr("style", "font-weight:bold")
-        .text("Divorced rate (%)");
-    // y axis title/label
-    var yLabel = svg.append("text")
-        .attr("class", "label")
-        .attr("transform", "rotate(-90)")
-        .attr("x", -height/2)
-        .attr("y", -37)
-        .attr("dy", ".71em")
-        .attr("style", "font-weight:bold")
-        .text("Depression rate (%)");
+        // scatterplot dots
+        svg.selectAll(".dot")
+            .data(data)
+            .enter().append("circle")
+            .attr("class", "dot")
+            .attr("r", 10)
+            .attr("cx", function(d){
+                return xScale(d[x_data]);
+            })
+            .attr("cy", function(d){
+                return yScale(d[y_data])
+            })
+            .attr("fill", "red")
+            .style("opacity", 0.5);
+
+        // append text to each point
+        var text = svg.selectAll("dot")
+            .data(data)
+            .enter()
+            .append("text");
+
+        var textLabels = text
+            .attr("class", "label")
+            .attr("x", function(d){
+                return xScale(d[x_data])-8;
+            })
+            .attr("y", function(d){
+                return yScale(d[y_data])+4;
+            })
+            .text(function(d){
+                console.log(d.State);
+                return d.State;
+            })
+            .style("font-size", "10px")
+            .style("font-weight", "bold")
+            .style("font-family", "verdana")
+            .style("opacity", 0.6);
+    } // end plot_data function
+
 }); // end d3.csv 
 
